29ab7503056e7bac5242756399565887289df5f8
Sachin Rajput <digitalrajput007@gmail.com>
2025-09-21 09:49:51 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -124,13 +124,13 @@ export const tools: Tool[] = [
 
 export const AppLogo = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
+    {...props}
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
-    {...props}
   >
     <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
     <polyline points="14 2 14 8 20 8" />
@@ -138,6 +138,7 @@ export const AppLogo = (props: React.SVGProps<SVGSVGElement>) => (
     <path d="m13.6 12.8 2.4 2.4-2.4 2.4" />
   </svg>
 );
+
 
 export const AppName = 'Online JPG PDF';
 export const ShareIcon = Share2;
