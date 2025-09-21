dea8b5274605d94b52e507fbcc9c87064bf392c9
Sachin Rajput <digitalrajput007@gmail.com>
2025-09-21 17:17:07 +0000

    Use the same icon as the website logo to show as favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={`${poppins.variable} ${ptSans.variable} ${greatVibes.variable} ${dancingScript.variable}`}>
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
